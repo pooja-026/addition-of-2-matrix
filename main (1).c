0f3e4e60c02c852da6b5e746e1df30aad2ed9595
pooja-026 <114637656+pooja-026@users.noreply.github.com>
2022-10-14 09:24:44 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+int main(){
+    int a[3][3],b[3][3],p[9],c[3][3],i,j;
+    printf("enter the elements of first array\n");
+    for(i=0;i<3;i++)
+{
+    for(j=0;j<3;j++)
+    {
+        scanf("%d", &a[i][j]);
+    }
+}
+printf("enter the elements of second array\n");
+ for(i=0;i<3;i++)
+{
+    for(j=0;j<3;j++)
+    {
+        scanf("%d", &b[i][j]);
+    }
+}
+  printf("addition of two arrays is\n");
+  for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            *(*(c+ i) + j) =(*(*(a+ i) + j))+(*(*(b + i) + j));
+        }
+    }
+    for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+                printf("%d\t",*(*(c + i) + j));
+            printf("\n");
+        }
+    return 0;
+}
